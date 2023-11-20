a8b4c4239753d3af537bf9a58fd1e9660c1fc748
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-11-20 23:57:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
 import React from "react";
+import Tab from './Tab';
 import './../styles/App.css';
-
+const tabData = [
+  {
+    title: 'Tab 1',
+    content: 'This is the content for Tab 1.',
+  },
+  {
+    title: 'Tab 2',
+    content: 'This is the content for Tab 2.',
+  },
+  {
+    title: 'Tab 3',
+    content: 'This is the content for Tab 3.',
+  },
+];
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+             <Tab tabData={tabData}/>
     </div>
   )
 }
